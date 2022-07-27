fa37e786a3a012e72fbca20b1bbe18f2b1c3627b
Andrey Ivanov <prioritet2503@gmail.com>
2022-07-27 17:14:48 +0300

    save

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ContactForm from './ContactForm';
 import styled from 'styled-components';
+import shortid from 'shortid';
 
 
-class Phonebook extends React.Component {
+class Phonebook extends Component {
     state = {
-        contacts: [],
-        
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ]  
     };
 
-    formSubmitHandler = data => {
-        console.log(data);
-        
+    contactId = shortid.generate();
+
+    formSubmitHandler = ({ data }) => {
+        this.setState(contacts => {
+            const newContact = {
+                id: 'id-5',
+                data
+            };
+            return {
+                contacts: [newContact, ...contacts],
+            };
+        })
     };
     
     
@@ -26,14 +40,12 @@ class Phonebook extends React.Component {
                 
                 <section>
                     <ul>
-                        <li>
-                            <span></span>
-                            <p ></p>
-                        </li>
-                        <li >
-                            <span ></span>
-                            <p></p>
-                        </li>
+                        {this.state.contacts.map(({ id, name, number }) => (
+                     <li key={id}>
+                        <p>{name}:</p>
+                        <p>{number}</p>
+                    </li>
+                    ))}
                     </ul>
                 </section>
         
