58f7ba505e54ff9376b147350b3d9174481ef60c
Andrey Ivanov <prioritet2503@gmail.com>
2022-07-27 16:07:44 +0300

    save

---
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+// import shortid from 'shortid';
+
+class ContactForm extends Component{
+    state = {
+        name: '',
+        number: ''
+    };
+
+    // nameInputId = shortid.generate();
+
+    onInputChange = e => {
+        const { name, value } = e.currentTarget;
+            this.setState({[name] : value})
+    };
+
+    onSubmitForm = e => {
+        e.preventDefault();
+        this.props.onSubmit(this.state);
+        this.reset()
+    };
+
+    reset = () => {
+        this.setState({name: '',
+        number: ''})
+    };
+
+    render() {
+        return (<form onSubmit={this.onSubmitForm}>
+                    <label>Name
+                    <input
+                        type="text"
+                        name="name"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                        value={this.state.name}
+                    onChange={this.onInputChange}
+                    // id={this.nameInputId}
+                        />
+                    </label>
+                    <label>Number
+                    <input
+                         type="tel"
+                        name="number"
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
+                            value={this.state.number}
+                            onChange={this.onInputChange}
+                        />
+                        </label>                    
+                    <button type="submit">Add contact</button>
+                </form>);
+    }
+}
+
+export default ContactForm;
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,50 +1,30 @@
 import React from 'react';
+import ContactForm from './ContactForm';
 import styled from 'styled-components';
 
 
 class Phonebook extends React.Component {
     state = {
         contacts: [],
-        name: '',
-        number: ''
+        
     };
 
-    onInputChange = e => {
-        const { name, value } = e.currentTarget;
-            this.setState({[name] : value})
+    formSubmitHandler = data => {
+        console.log(data);
+        
     };
+    
     
     render() {
         return (
             <SectionPhonebook>
-                <form>
-                    <h2>Phonebook</h2>
-                    <label>Name
-                    <input
-                        type="text"
-                        name="name"
-                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                        required
-                        value={this.state.name}
-                        onChange={this.onInputChange}
-                        />
-                    </label>
-                    <label>Number
-                    <input
-                         type="tel"
-                        name="number"
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                        required
-                            value={this.state.number}
-                            onChange={this.onInputChange}
-                        />
-                        </label>                    
-                    <button type="submit">Add contact</button>
-                </form>
+                <h1>Phonebook</h1>
+                <ContactForm onSubmit={ this.formSubmitHandler} />
+                <h2>Contacts</h2>
+                {/* <Filter/>
+                <ContactList/> */}
+                
                 <section>
-                    <h2>Contacts</h2>
                     <ul>
                         <li>
                             <span></span>
@@ -63,7 +43,7 @@ class Phonebook extends React.Component {
 };
 
 const SectionPhonebook = styled.div`
-width: 90px;
+width: 200px;
 margin: auto;
 `
 
